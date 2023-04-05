557a27469f3dd7e5dbba22e5a68f539596f775ac
Jakob Abou Zeid <jakob.abouzeid@gmail.com>
2023-04-05 15:14:36 +0200

    started implementing the Neuron-class

--- 001_singleNeuron/main.py
+++ 001_singleNeuron/main.py
@@ -3,6 +3,20 @@
 
 
 # We already assembled the dataset in our Jupyter Notebook and can just load it here.
+import typing
+in_type = NewType("Neuron", Neuron)
+import numpy as np
+
+class Neuron:
+    next_neuron_ID = 0
+    def __init__(self, inputs: typing.List[Neuron]) -> None:
+        parameters = {}
+        for input_val in inputs:
+            parameters[input_val.neuron_ID] = np.random.rand()
+
+        
+        self.neuron_ID = Neuron.next_neuron_ID
+        Neuron.next_neuron_ID +=1
 
 
 
