4190721221debe2a3becf43d9eebbf36587059f7
Jakob Abou Zeid <jakob.abouzeid@gmail.com>
2023-04-05 12:10:51 +0200

    Added file to compile the dataset

--- 001_singleNeuron/main.py
+++ 001_singleNeuron/main.py
@@ -1,1 +1,9 @@
-# This is my first py-file that I created from the command-line and opened in VS Code afterwards. I feel very proud :) 
+# @ Author: Jakob Abou Zeid
+# @ email: jakob.abouzeid@gmail.com
+
+
+# We already assembled the dataset in our Jupyter Notebook and can just load it here.
+
+
+
+
